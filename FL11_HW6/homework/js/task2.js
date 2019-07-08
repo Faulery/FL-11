b710890f465ef2db88266d8a9ff8f36964d6433d
Yurii Ilnytskyi <yurii.ilnytskyi@gmail.com>
2019-07-08 18:24:27 +0300

    Fixed

--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -2,7 +2,7 @@ let a =+ prompt('Enter your a coordinate for triangle sides length', '');
 let b =+ prompt('Enter your b coordinate for triangle sides length', '');
 let c =+ prompt('Enter your c coordinate for triangle sides length', '');
 
-if (a+b > c && a+c > b && b+c > a && (a > 0 && b > 0 && c > 0)) {
+if (a+b > c || a+c > b || b+c > a || (a > 0 && b > 0 && c > 0)) {
   if (a === b && a === c && b === c) {
     console.log('Equivalent triangle');
   } else if (a === b && b === c || a === c && a === b || b === c && c === a) {
