6e5bc1302314521f7172f386949283b0bcf30b1a
Yurii Ilnytskyi <yurii.ilnytskyi@gmail.com>
2019-07-28 18:30:35 +0300

    Homework #12

--- FL11_HW12/homework/src/app.js
+++ FL11_HW12/homework/src/app.js
@@ -1,30 +1,44 @@
 const rootNode = document.getElementById('root');
-let todoItems = [];
+const alertTimeout = 3000;
+let actions = [];
 let storage = {
   add(description) {
-    let id = 'id_' + +new Date();
-    let item = {id, description, isDone: false};
-    todoItems.push(item);
-    localStorage.setItem('todoItems', JSON.stringify(todoItems));
+    const ID = 'id_' + +new Date();
+    let item = {id: ID, description, isDone: false};
+    actions.push(item);
+    localStorage.setItem('actions', JSON.stringify(actions));
 
-    return todoItems;
+    return actions;
   },
 
   getItems() {
-    return JSON.parse(localStorage.getItem('todoItems'));
+    return JSON.parse(localStorage.getItem('actions'));
   },
 
   setAsDoneById(id) {
-    const updatedList = this.getItems().map(item => {
+    let updatedList = this.getItems().map(item => {
       if (item.id === id) {
         item.isDone = true;
       }
 
       return item;
     });
-    localStorage.setItem('todoItems', JSON.stringify(updatedList));
+    localStorage.setItem('actions', JSON.stringify(updatedList));
 
-    return todoItems;
+    return actions;
+  },
+
+  setAsUnDoneById(id) {
+    let updatedList = this.getItems().map(item => {
+      if (item.id === id) {
+        item.isDone = false;
+      }
+
+      return item;
+    });
+    localStorage.setItem('actions', JSON.stringify(updatedList));
+
+    return actions;
   },
 
   getStatus(value) {
@@ -36,30 +50,29 @@ let storage = {
   },
 
   editDescription(id, description) {
-    const updatedList = this.getItems().map(item => {
+    let updatedList = this.getItems().map(item => {
       if (item.id === id) {
         item.description = description;
       }
 
       return item;
     });
-    localStorage.setItem('todoItems', JSON.stringify(updatedList));
+    localStorage.setItem('actions', JSON.stringify(updatedList));
 
-    return todoItems;
+    return actions;
   },
 
   removeById(id) {
-    const updatedList = this.getItems().filter(item => item.id !== id);
-    localStorage.setItem('todoItems', JSON.stringify(updatedList));
+    let updatedList = this.getItems().filter(item => item.id !== id);
+    localStorage.setItem('actions', JSON.stringify(updatedList));
 
-    return todoItems;
+    return actions;
   }
 };
 
-
 window.onload = window.onhashchange = () => {
-  if (localStorage.getItem('todoItems')) {
-    todoItems = storage.getSorted();
+  if (localStorage.getItem('actions')) {
+    actions = storage.getSorted();
   }
 
   if (window.location.hash === '#/add') {
@@ -67,76 +80,77 @@ window.onload = window.onhashchange = () => {
     rootNode.innerHTML = '';
     rootNode.appendChild(add());
   } else if (window.location.hash.includes('/modify')) {
-    const id = window.location.hash.slice(window.location.hash.lastIndexOf('/') + 1);
-    const item = JSON.parse(localStorage.getItem('todoItems')).find(item => item.id === id);
+    let id = window.location.hash.slice(window.location.hash.lastIndexOf('/') + 1);
+    let item = JSON.parse(localStorage.getItem('actions')).find(item => item.id === id);
     document.title = 'Modify item';
     rootNode.innerHTML = '';
     rootNode.appendChild(modify(item));
   } else {
     document.title = 'Main page';
     rootNode.innerHTML = '';
-    rootNode.appendChild(main(todoItems));
+    rootNode.appendChild(main(actions));
   }
 };
 
-function setAttributes(elem, attributes){
-  for(let key in attributes){
-    if (attributes.hasOwnProperty(key)){
+function setAttributes(elem, attributes) {
+  for (let key in attributes) {
+    if (attributes.hasOwnProperty(key)) {
       elem.setAttribute(key, attributes[key]);
     }
   }
 }
 
 function main(todoItems) {
-  const section = document.createElement('section');
-  setAttributes(section, {id: 'main_section'});
+  let view = document.createElement('section');
+  setAttributes(view, {id: 'main_section'});
 
-  const header = document.createElement('h1');
-  const headText = document.createTextNode('Simple TODO application');
-  header.appendChild(headText);
+  let header = document.createElement('h1');
+  header.innerText = 'Simple TODO application';
 
-  const addNewTaskBtn = document.createElement('button');
-  setAttributes(addNewTaskBtn, {id: 'add_new_task_btn'});
-  const addBtnText = document.createTextNode('Add new task');
-  addNewTaskBtn.appendChild(addBtnText);
+  let addAction = document.createElement('button');
+  setAttributes(addAction, {id: 'add_new_task_btn'});
+  addAction.innerText = 'Add new task';
 
-  const todoList = document.createElement('ul');
-  setAttributes(todoList, {id: 'todo-list'});
+  let todoList = document.createElement('ul');
+  setAttributes(todoList, {id: 'todo_list'});
 
-  const initialList = document.createElement('p');
-  setAttributes(initialList, {class: 'empty_todo'});
-  const initialText = document.createTextNode('TODO is empty');
-  initialList.appendChild(initialText);
+  let init = document.createElement('p');
+  setAttributes(init, {class: 'empty_todo'});
+  init.innerText = 'TODO is empty';
 
-  addNewTaskBtn.onclick = () => {
+  addAction.onclick = () => {
     window.location.hash = '/add';
   };
 
-  section.appendChild(header);
-  section.appendChild(addNewTaskBtn);
-  section.appendChild(todoList);
-  section.appendChild(initialList);
+  view.appendChild(header);
+  view.appendChild(addAction);
+  view.appendChild(todoList);
+  view.appendChild(init);
 
   if (todoItems.length) {
     for (let item of todoItems) {
-      const li = document.createElement('li');
+      let li = document.createElement('li');
       setAttributes(li, {id: item.id});
 
-      const checkbox = document.createElement('button');
-      setAttributes(checkbox, {class: item.isDone ? 'checkbox_checked' : 'checkbox_empty'});
+      let checkbox = document.createElement('button');
+      setAttributes(checkbox, {class: item.isDone ? 'checked' : 'unchecked'});
 
-      const todoText = document.createElement('button');
+      let todoText = document.createElement('button');
       setAttributes(todoText, {class: 'todo_text', title: 'Click to edit'});
-      const todoTextDesc = document.createTextNode(item.description);
-      todoText.appendChild(todoTextDesc);
+      todoText.innerText = item.description;
 
-      const removeItem = document.createElement('button');
-      setAttributes(removeItem, {class: 'remove_item'});
+      let removeAction = document.createElement('button');
+      setAttributes(removeAction, {class: 'remove_item'});
 
       checkbox.onclick = () => {
-        if (checkbox.className === 'checkbox_empty') {
-          checkbox.className = 'checkbox_checked';
+        if (checkbox.className === 'unchecked') {
+          checkbox.className = 'checked';
           storage.setAsDoneById(item.id);
+          location.reload();
+          todoList.appendChild(li);
+        } else {
+          checkbox.className = 'unchecked';
+          storage.setAsUnDoneById(item.id);
           location.reload();
           todoList.appendChild(li);
         }
@@ -145,30 +159,37 @@ function main(todoItems) {
       todoText.onclick = () => {
         if (item.isDone) {
           window.location.hash = '';
+          let block = document.createElement('div');
+          setAttributes(block, {class: 'alert'});
+          block.innerHTML = '<p>You can not edit already done action</p>';
+          rootNode.appendChild(block);
+          setTimeout(function () {
+            setAttributes(block, {class: 'hide'});
+          }, alertTimeout);
         } else {
           window.location.hash = `#/modify/${item.id}`;
         }
       };
 
-      removeItem.onclick = () => {
+      removeAction.onclick = () => {
         li.remove();
         storage.removeById(item.id);
       };
 
       li.appendChild(checkbox);
       li.appendChild(todoText);
-      li.appendChild(removeItem);
+      li.appendChild(removeAction);
 
       todoList.appendChild(li);
     }
   }
 
-  return section;
+  return view;
 }
 
 function add() {
-  let section = document.createElement('section');
-  setAttributes(section, {id: 'add_screen'});
+  let view = document.createElement('section');
+  setAttributes(view, {id: 'add_screen'});
 
   let header = document.createElement('h1');
   header.innerText = 'Add task';
@@ -178,53 +199,51 @@ function add() {
 
   let footer = document.createElement('footer');
 
-  let cancelBtn = document.createElement('button');
-  setAttributes(cancelBtn, {class: 'cancel_btn'});
-  let cancelText = document.createTextNode('Cancel');
-  cancelBtn.appendChild(cancelText);
+  let cancel = document.createElement('button');
+  setAttributes(cancel, {class: 'cancel_btn'});
+  cancel.innerText = 'Cancel';
 
-  let saveBtn = document.createElement('button');
-  setAttributes(saveBtn, {class: 'save_btn', disabled: 'true'});
-  let saveText = document.createTextNode('Save changes');
-  saveBtn.appendChild(saveText);
+  let save = document.createElement('button');
+  setAttributes(save, {class: 'save_btn', disabled: 'true'});
+  save.innerText = 'Save';
 
   input.onchange = input.onkeyup = () => {
     let inputValue = input.value.trim();
-    saveBtn.disabled = !inputValue;
+    save.disabled = !inputValue;
 
     if (inputValue && event.code === 'Enter') {
-      saveBtn.click();
+      save.click();
     }
   };
 
-  cancelBtn.onclick = () => {
+  cancel.onclick = () => {
     window.location.hash = '';
   };
 
-  saveBtn.onclick = () => {
+  save.onclick = () => {
     storage.add(input.value);
     window.location.hash = '';
   };
 
-  footer.appendChild(cancelBtn);
-  footer.appendChild(saveBtn);
+  footer.appendChild(cancel);
+  footer.appendChild(save);
 
-  section.appendChild(header);
-  section.appendChild(input);
-  section.appendChild(footer);
+  view.appendChild(header);
+  view.appendChild(input);
+  view.appendChild(footer);
 
-  return section;
+  return view;
 }
 
 function modify(item) {
-  const section = this.add();
+  let view = this.add();
 
-  section.querySelector('h1').textContent = 'Modify item';
-  section.querySelector('input').value = item.description;
-  section.querySelector('.save_btn').onclick = () => {
-    storage.editDescription(item.id, section.querySelector('input').value);
+  view.querySelector('h1').textContent = 'Modify item';
+  view.querySelector('input').value = item.description;
+  view.querySelector('.save_btn').onclick = () => {
+    storage.editDescription(item.id, view.querySelector('input').value);
     window.location.hash = '';
   };
 
-  return section;
+  return view;
 }
